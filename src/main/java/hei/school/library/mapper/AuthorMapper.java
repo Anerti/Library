195b87f25864797981c756eea2f2a2dd8ed173fd
Ervin ANDRIANOMBANA <e.andrianombana@protonmail.com>
2026-06-07 16:20:53 +0300

    feat: add toPageResponse method to AuthorMapper

--- src/main/java/hei/school/library/mapper/AuthorMapper.java
+++ src/main/java/hei/school/library/mapper/AuthorMapper.java
@@ -1,7 +1,10 @@
 package hei.school.library.mapper;
 
 import hei.school.library.dto.AuthorResponse;
+import hei.school.library.dto.PageResponse;
+import hei.school.library.dto.PaginationDto;
 import hei.school.library.entity.Author;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -12,6 +15,17 @@ public class AuthorMapper {
         .id(author.getId())
         .firstName(author.getFirstName())
         .lastName(author.getLastName())
+        .build();
+  }
+
+  public PageResponse<AuthorResponse> toPageResponse(Page<Author> page, int pageNum, int pageSize) {
+    return PageResponse.<AuthorResponse>builder()
+        .data(page.getContent().stream().map(this::toResponse).toList())
+        .pagination(PaginationDto.builder()
+            .page(pageNum)
+            .size(pageSize)
+            .total(page.getTotalElements())
+            .build())
         .build();
   }
 }
