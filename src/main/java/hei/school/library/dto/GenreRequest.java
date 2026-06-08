3cf4f609de480ae105ddbb3da87fe0b261ab79cb
Mikaiah <mikaiahranaivoson@gmail.com>
2026-06-08 16:43:32 +0300

    feat: use lombok for every class

---
+++ src/main/java/hei/school/library/dto/GenreRequest.java
@@ -0,0 +1,12 @@
+package hei.school.library.dto;
+
+import lombok.*;
+
+@Builder
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class GenreRequest {
+    private String name;
+}
---
+++ src/main/java/hei/school/library/validator/GenreValidator.java
@@ -0,0 +1,14 @@
+package hei.school.library.validator;
+
+import hei.school.library.dto.GenreRequest;
+import hei.school.library.exception.BadRequestException;
+import hei.school.library.exception.ConflictException;
+import org.springframework.stereotype.Component;
+@Component
+public class GenreValidator {
+    public void isExistByName(boolean isExist) {
+        if (isExist) {
+            throw new ConflictException("The requested resource already exists");
+        }
+    }
+}
