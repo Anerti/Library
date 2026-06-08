78750d278b915172d267bae8a376fff827ee2893
Mikaiah <mikaiahranaivoson@gmail.com>
2026-06-08 18:58:06 +0300

    fix: optimize saving request

--- src/main/java/hei/school/library/repository/dao/GenreRepository.java
+++ src/main/java/hei/school/library/repository/dao/GenreRepository.java
@@ -2,8 +2,22 @@ package hei.school.library.repository.dao;
 
 import hei.school.library.entity.Genre;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
 import java.util.UUID;
 
 public interface GenreRepository extends JpaRepository<Genre, UUID> {
-    boolean existsByNameIgnoreCase(String name);
+    @Query(
+            value = """
+          INSERT INTO genre (id, name, created_at, updated_at)
+          VALUES (gen_random_uuid(), :name, NOW(), NOW())
+          ON CONFLICT (name) DO NOTHING
+          RETURNING *
+          """,
+            nativeQuery = true)
+    Optional<Genre> insertGenreIgnoreConflict(
+            @Param("name") String name
+    );
 }
--- src/test/java/hei/school/library/service/genre/GenreServiceTest.java
+++ src/test/java/hei/school/library/service/genre/GenreServiceTest.java
@@ -1,6 +1,7 @@
 package hei.school.library.service.genre;
 
 import hei.school.library.dto.GenreRequest;
+import hei.school.library.dto.GenreResponse;
 import hei.school.library.entity.Genre;
 import hei.school.library.exception.ConflictException;
 import hei.school.library.exception.UnprocessableEntityException;
@@ -8,17 +9,16 @@ import hei.school.library.mapper.GenreMapper;
 import hei.school.library.repository.dao.GenreRepository;
 import hei.school.library.service.GenreService;
 import hei.school.library.validator.DataValidator;
-import hei.school.library.validator.GenreValidator;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import java.time.Instant;
+import java.util.Optional;
 import java.util.UUID;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.when;
 
@@ -30,47 +30,56 @@ public class GenreServiceTest {
     private DataValidator dataValidator;
     @Mock
     private GenreMapper genreMapper;
-    @Mock
-    private GenreValidator genreValidator;
 
     private GenreService genreService;
 
     @BeforeEach
     void setUp() {
-        genreService = new GenreService(genreRepository, genreMapper, genreValidator, dataValidator);
+        genreService = new GenreService(genreRepository, genreMapper, dataValidator);
     }
 
     @Test
     void should_create_genre() throws Exception {
 
         UUID id = UUID.randomUUID();
+        String genreName = "Fantasy";
+
         Genre genre = Genre.builder()
                 .id(id)
-                .name("Fantasy")
+                .name(genreName)
                 .createdAt(Instant.now())
                 .updatedAt(Instant.now())
                 .build();
 
-        when(genreRepository.save(any(Genre.class))).thenReturn(genre);
+        GenreResponse expectedResponse = GenreResponse.builder()
+                .id(id)
+                .name(genreName)
+                .build();
 
-        assertEquals(genreMapper.toResponse(genre), genreService.createGenreByName(
-                GenreRequest.builder()
-                .name("Fantasy")
-                .build()));
+        when(genreRepository.insertGenreIgnoreConflict(genreName))
+                .thenReturn(Optional.of(genre));
+        when(genreMapper.toResponse(genre)).thenReturn(expectedResponse);
+
+        GenreResponse actualResponse = genreService.createGenreByName(
+                GenreRequest.builder().name(genreName).build()
+        );
+        assertEquals(expectedResponse, actualResponse);
     }
     @Test
     void should_return_conflict_when_genre_already_exists() throws Exception {
-        when(genreRepository.existsByNameIgnoreCase("Fantasy")).thenReturn(true);
-        doThrow(ConflictException.class).when(genreValidator).isExistByName(true);
+        String genreName = "Fantasy";
+        when(genreRepository.insertGenreIgnoreConflict(genreName))
+                .thenReturn(Optional.empty());
         assertThrows(ConflictException.class, () -> genreService.createGenreByName(
                 GenreRequest.builder()
-                .name("Fantasy")
-                .build()));
+                        .name(genreName)
+                        .build()
+        ));
     }
     @Test
     void should_return_unprocessable_entity_exception_when_request_is_not_valid() throws Exception {
         doThrow(UnprocessableEntityException.class)
-                .when(dataValidator).validateString("name", null);
+                .when(dataValidator).validateName("name", null);
 
         assertThrows(UnprocessableEntityException.class, () -> genreService.createGenreByName(
                 GenreRequest.builder()
