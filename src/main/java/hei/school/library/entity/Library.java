cb0127ba1ca56dcd3e42b68cd37f5e4c15ffc9f4
Andraina <hei.andraina.4@gmail.com>
2026-06-08 23:28:31 +0300

    feat: add arrivalFindByIdServiceTest and arrivalFindByLibraryIdServiceTest

--- src/main/java/hei/school/library/entity/Library.java
+++ src/main/java/hei/school/library/entity/Library.java
@@ -8,12 +8,14 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import java.util.UUID;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "library")
 @Data
+@Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class Library {
