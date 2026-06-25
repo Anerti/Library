9d959145ca7fdd2137a623852552c0d2ce4c4ff2
Ervin ANDRIANOMBANA <e.andrianombana@protonmail.com>
2026-06-25 15:38:42 +0300

    chore: reorganize db scripts into subdirectories

--- src/main/resources/db/genre_schema.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE IF NOT EXISTS genre (
-    id         UUID        PRIMARY KEY DEFAULT gen_random_uuid(),
-    name       VARCHAR(100) NOT NULL UNIQUE,
-    created_at TIMESTAMP   DEFAULT now(),
-    updated_at TIMESTAMP   DEFAULT now()
-);
--- src/main/resources/db/schema.sql
+++ src/main/resources/db/schema.sql
@@ -5,3 +5,9 @@ CREATE TABLE IF NOT EXISTS library (
     email   VARCHAR(100) NOT NULL UNIQUE,
     address VARCHAR(100) NOT NULL UNIQUE
 );
+
+CREATE TABLE IF NOT EXISTS genre (
+    id         UUID        PRIMARY KEY DEFAULT gen_random_uuid(),
+    name       VARCHAR(100) NOT NULL UNIQUE,
+);
+
