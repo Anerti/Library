656dcc5bbd0ff8802f1d8fa9694d5afe5c97f83c
Ervin ANDRIANOMBANA <e.andrianombana@protonmail.com>
2026-06-25 10:30:15 +0300

    feat: add library DDL and seed SQL scripts

---
+++ src/main/resources/db/seed_library.sql
@@ -0,0 +1,7 @@
+INSERT INTO library (id, name, phone, email, address) VALUES
+    ('43af5aff-055d-4af6-a708-4d165ad7abac', 'Bibliothèque Municipale',       '+261 34 12 345 67', 'contact@biblio-municipale.mg',   '15 Avenue de l''Indépendance, Antananarivo'),
+    ('1e9a43cd-2e6a-409d-8b5a-d3f57a2036f0', 'Tech Library',                 '+261 32 01 234 56', 'tech@library.com',              '456 Avenue Technologique'),
+    ('c70a6c6b-6145-40db-b230-031db0fbfd66', 'Librairie Générale',           '+261 33 98 765 43', 'info@librairie-generale.mg',    '22 Rue des Livres, Antananarivo'),
+    ('227b8fbc-f13f-4cfc-9774-5f6ddc1328f0', 'St. Martin''s Library',        '+261 20 11 223 34', 'saint@martin-library.org',      '1'' Rue Saint-Martin'),
+    ('2639ad05-3715-4bb1-9edb-26ff8030f9fe', 'Médiathèque Centrale',         '+261 34 56 789 01', 'mediatheque@centrale.mg',       '8 Place de la Culture'),
+    ('8c0bf175-f484-46f6-8fbf-de369fde450f', 'Bibliothèque Universitaire',   '+261 32 55 44 332', 'uni@bibliotheque-univ.mg',      'Campus Universitaire BP 123');
