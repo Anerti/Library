f1e5309c2a9157393ff9e9acbfae1d72279caa8f
Ervin ANDRIANOMBANA <e.andrianombana@protonmail.com>
2026-06-25 14:35:33 +0300

    feat: implement DELETE /libraries/{libraryId} endpoint

--- src/main/java/hei/school/library/endpoint/rest/controller/LibraryController.java
+++ src/main/java/hei/school/library/endpoint/rest/controller/LibraryController.java
@@ -4,10 +4,13 @@ import hei.school.library.dto.LibraryListResponse;
 import hei.school.library.dto.LibraryRequest;
 import hei.school.library.dto.LibraryResponse;
 import hei.school.library.service.LibraryService;
+import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -34,5 +37,11 @@ public class LibraryController {
   @PostMapping
   public ResponseEntity<LibraryResponse> create(@RequestBody LibraryRequest request) {
     return ResponseEntity.status(HttpStatus.CREATED).body(libraryService.createLibrary(request));
+  }
+
+  @DeleteMapping("/{libraryId}")
+  public ResponseEntity<Void> delete(@PathVariable UUID libraryId) {
+    libraryService.deleteLibrary(libraryId);
+    return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
   }
 }
